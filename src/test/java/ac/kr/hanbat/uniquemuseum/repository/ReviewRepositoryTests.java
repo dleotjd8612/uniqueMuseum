7d34e54a77ef44c53b38949ef8dd1168d4c1be70
dleotjd8612 <dleotjd8612@naver.com>
2021-11-08 14:04:40 +0900

    리뷰 등록 테스트 코드 추가

---
+++ src/test/java/ac/kr/hanbat/uniquemuseum/repository/ReviewRepositoryTests.java
@@ -0,0 +1,33 @@
+package ac.kr.hanbat.uniquemuseum.repository;
+
+import ac.kr.hanbat.uniquemuseum.entity.Member;
+import ac.kr.hanbat.uniquemuseum.entity.Museum;
+import ac.kr.hanbat.uniquemuseum.entity.Review;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class ReviewRepositoryTests {
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Test
+    public void insertMuseumReviews() {
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Long mno = (long)(Math.random() * 100) + 1;
+            Member member = Member.builder().email("r" + i + "@hanbat.ac.kr").build();
+
+            Review review = Review.builder()
+                    .member(member)
+                    .museum(Museum.builder().mno(mno).build())
+                    .grade((int)(Math.random() * 5) + 1)
+                    .text("이 영화에 대한 느낌이랄까.." + i)
+                    .build();
+
+            reviewRepository.save(review);
+        });
+    }
+}

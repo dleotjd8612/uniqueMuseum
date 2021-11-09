1fcdca076360fd6b894f7fee3f69ee4ed961460d
dleotjd8612 <dleotjd8612@naver.com>
2021-11-09 16:46:20 +0900

    실제 저장된 경로 설정 소스 추가

--- src/main/java/ac/kr/hanbat/uniquemuseum/controller/UploadController.java
+++ src/main/java/ac/kr/hanbat/uniquemuseum/controller/UploadController.java
@@ -1,6 +1,7 @@
 package ac.kr.hanbat.uniquemuseum.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,6 +9,9 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @Log4j2
 public class UploadController {
+
+    @Value("${ac.kr.hanbat.upload.path}") // application.properties의 변수
+    private String uploadPath;
 
     @PostMapping("/uploadAjax")
     public void uploadFile(MultipartFile[] uploadFiles) { // 파일 업로드 예시

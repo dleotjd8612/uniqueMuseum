361d448fae72354afa6573338baf2f5e2934e277
dleotjd8612 <dleotjd8612@naver.com>
2021-11-10 10:18:06 +0900

    업로드 결과 반환과 화면 처리 추가

--- src/main/java/ac/kr/hanbat/uniquemuseum/controller/UploadController.java
+++ src/main/java/ac/kr/hanbat/uniquemuseum/controller/UploadController.java
@@ -1,7 +1,10 @@
 package ac.kr.hanbat.uniquemuseum.controller;
 
+import ac.kr.hanbat.uniquemuseum.dto.UploadResultDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +16,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -24,38 +29,37 @@ public class UploadController {
 
     @PreAuthorize("permitAll()")
     @PostMapping("/uploadAjax")
-    public void uploadFile(MultipartFile[] uploadFiles) { // 파일 업로드 예시
+    public ResponseEntity<List<UploadResultDTO>> uploadFile(MultipartFile[] uploadFiles) { // 파일 업로드 예시
+        List<UploadResultDTO> resultDTOList = new ArrayList<>();
         for(MultipartFile file : uploadFiles) {
-
-            // 이미지 파일만 업로드 가능(검사)
-            if(file.getContentType().startsWith("image") == false) {
-                log.warn("this file is not image type");
-                return;
+            try {
+                // 이미지 파일만 업로드 가능(검사)
+                if(file.getContentType().startsWith("image") == false) {
+                    log.warn("this file is not image type");
+                    return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+                }
+            }catch (NullPointerException e) {
+                e.printStackTrace();
             }
-            
             // 실제 파일 이름 IE나 Edge는 전체 경로가 들어오므로
             String originalName = file.getOriginalFilename();
             String fileName = originalName.substring(originalName.lastIndexOf("\\") + 1);
-
             log.info("fileName: " + fileName);
-
             // 날짜 폴더 생성
             String folderPath = makeFolder();
-
             // UUID 생성
             String uuid = UUID.randomUUID().toString();
-
             //저장할 파일 이름 중간에 "_"를 이용해서 구분
             String saveName = uploadPath + File.separator + folderPath + File.separator + uuid + "_" + fileName;
-
             Path savePath = Paths.get(saveName);
-
             try {
-                file.transferTo(savePath);
+                file.transferTo(savePath); // 실제 이미지 저장
+                resultDTOList.add(new UploadResultDTO(fileName, uuid, folderPath));
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+        return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
     }
 
     private String makeFolder() {
--- src/main/java/ac/kr/hanbat/uniquemuseum/dto/UploadResultDTO.java
+++ src/main/java/ac/kr/hanbat/uniquemuseum/dto/UploadResultDTO.java
@@ -1,5 +1,25 @@
 package ac.kr.hanbat.uniquemuseum.dto;
 
-public class UploadResultDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+@Data
+@AllArgsConstructor
+public class UploadResultDTO implements Serializable {
+    private String fileName;
+    private String uuid;
+    private String folderPath;
+
+    public String getImageURL() { // 이미지 전체 경로
+        try {
+            return URLEncoder.encode(folderPath + "/" + uuid + "_" + fileName, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
 }

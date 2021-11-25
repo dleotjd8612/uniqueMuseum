9b09c609c717bbbc27ac08bb49fdf6e8e8121f80
ds <dleotjd8612@gmail.com>
2021-11-26 00:49:57 +0900

    쿠키를 이용한 7일간 자동 로그인 기능 추가

--- src/main/java/ac/kr/hanbat/uniquemuseum/config/SecurityConfig.java
+++ src/main/java/ac/kr/hanbat/uniquemuseum/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package ac.kr.hanbat.uniquemuseum.config;
 
 import ac.kr.hanbat.uniquemuseum.security.handler.ClubLoginSuccessHandler;
+import ac.kr.hanbat.uniquemuseum.security.service.ClubUserDetailsService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -16,11 +17,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Log4j2
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private ClubUserDetailsService userDetailsService;
+
     //  Bean이란 : Spring IoC 컨테이너가 관리하는 자바 객체를 빈(Bean)이라고 한다.
     //  @Component : 개발자가 직접 작성한 Class를 Bean으로 만드는 것
     //  @Bean : 개발자가 작성한 Method를 통해 반환되는 객체를 Bean으로 만드는 것
     @Bean
-    // PasswordEncoder: 비밀번호를 안전하게 저장할 수 있도록 비밀번호의 단방향 암호화를 지원
     PasswordEncoder passwordEncoder() {
         // BCryptPasswordEncoder(): BCrypt 해싱 함수(BCrypt hashing function)를 사용해서 비밀번호를 인코딩해주는 메서드와 사용자의 의해 제출된 비밀번호와 저장소에 저장되어 있는 비밀번호의 일치 여부를 확인
         return new BCryptPasswordEncoder();
@@ -29,31 +32,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // permitAll(): 모든 사용자에게 접근 허락, 비로그인 사용자(익명 사용자) 포함
-        http.authorizeRequests()
-                .antMatchers("/museum/list").permitAll()
-                .antMatchers("/museum/read").permitAll()
-                .antMatchers("/museum/register").hasRole("ADMIN")
-                .antMatchers("/museum/modify").hasRole("ADMIN");
-
-//                .antMatchers("/reviews/{mno}/all").hasRole("USER");
+//        http.authorizeRequests()
+//                .antMatchers("/museum/list").permitAll()
+//                .antMatchers("/museum/read").permitAll()
+//                .antMatchers("/museum/register").hasRole("ADMIN")
+//                .antMatchers("/museum/modify").hasRole("ADMIN");
 
         http.formLogin(); // 인가, 인증에 문제시 로그인 화면으로 이동
-
         http.csrf().disable(); // CSRF 토큰을 발행하지 않도록 설정
         http.logout() // 로그아웃 페이지로 이동
                 .logoutSuccessUrl("/museum/list");
-        
         http.oauth2Login().successHandler(successHandler()); // 구글 계정 로그인
+        http.rememberMe().tokenValiditySeconds(60*60*24*7).userDetailsService(userDetailsService); // 쿠키를 이용한 7일간 자동 로그인 기능
     }
 
     @Bean
     public ClubLoginSuccessHandler successHandler() {
         return new ClubLoginSuccessHandler(passwordEncoder());
     }
-
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//
-//    }
 
 }
